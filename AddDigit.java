431436c878c37cd92ac15d70f4f2add503f04149
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2025-08-31 23:59:47 +0530

    add digit

--- AddDigit.java
+++ AddDigit.java
@@ -1,25 +1,21 @@
 class Solution {
     public int addDigits(int num) {
-        int sum=0;
-        int r;
-        int s=0;
-        while(num>0){
-            r=num%10;
-            sum=r+sum;
-            num=num/10;
+        while (num >= 10) { 
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
         }
-        while(sum>0){
-            r=sum%10;
-            s=s+r;
-            sum=sum/10;
-        }
-        return s;
+        return num;
     }
 }
-public class AddDigit{
-    public static void main(String args[]){
-        Solution s=new Solution();
-        int num=38;
-        s.addDigits(num);
+
+public class AddDigit {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int num = 38;
+        System.out.println(s.addDigits(num)); 
     }
 }
