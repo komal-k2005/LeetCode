069c08a77f36a7be0fa723c79fd6dce6c7d6091d
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2025-09-22 00:06:15 +0530

    triboncci nth number

--- Triboncci.java
+++ Triboncci.java
@@ -1,6 +1,24 @@
 class Solution {
     public int tribonacci(int n) {
-        in f1
+        int f1=0;
+        int f2=1;
+        int f3=1;
+        int f4=f1+f2+f3;
+        if(n==0){
+            return 0;
+        }
+        else if(n==1 || n==2){
+            return 1;
+        }
+      else{
+          for(int i=2;i<n;i++){
+            f4=f1+f2+f3;
+            f1=f2;
+            f2=f3;
+            f3=f4;
+        }
+        return f4;
+      }
     }
 }
 public class Triboncci{
