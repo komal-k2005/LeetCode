529304426866bbbd3369ff461eec77a7661a12ae
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2025-09-21 23:45:38 +0530

    triboncci number

---
+++ Triboncci.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int tribonacci(int n) {
+        in f1
+    }
+}
+public class Triboncci{
+public static void main(String args[]){
+    Solution s=new Solution();
+    int n=4;
+    System.out.println(s.tribonacci(n));
+}
+}
