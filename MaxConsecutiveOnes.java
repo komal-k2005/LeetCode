264b6742aadafdf8a2241d16703a4159d125262e
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-01-18 01:46:27 +0530

    Create MaxConsecutiveOnes.java

---
+++ MaxConsecutiveOnes.java
@@ -0,0 +1,24 @@
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max=0;
+        int count=0;
+        int last=nums.length-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]== 1){
+                count++;
+            }
+            else if(nums[i]==0){
+                if(count>max){
+                    max=count;
+                }
+                count=0;
+            }
+           
+        }
+        if(count>max){
+             max=count;
+                }
+        return max;
+    }
+}
+
