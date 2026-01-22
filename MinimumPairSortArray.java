c025de5468f933113cbf4b9ad8d2b9ce0da610fd
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-01-22 16:15:04 +0530

    Create MinimumPairSortArray.java

---
+++ MinimumPairSortArray.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class MinimumPairSortArray {
+    public int minimumPairRemoval(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int x : nums) list.add(x);
+        int operations = 0;
+        while (!isNonDecreasing(list)) {
+            int minSum = Integer.MAX_VALUE;
+            System.out.println(minSum);
+            int idx = 0;
+            for (int i = 0; i < list.size() - 1; i++) {
+                int sum = list.get(i) + list.get(i + 1);
+                if (sum < minSum) {
+                    minSum = sum;
+                    idx = i;
+                }
+            }
+            list.set(idx, minSum);
+            list.remove(idx + 1);
+            operations++;
+        }
+        return operations;
+    }
+
+    private boolean isNonDecreasing(List<Integer> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) < list.get(i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
