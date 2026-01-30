197a41da4f630357a29cc0447f435aac4319da25
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-01-30 20:19:32 +0530

    Create MaximumProductThree.java

---
+++ MaximumProductThree.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class MaximumProductThree {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int product1=0;
+        int product2=0;
+        int n=nums.length;
+        product1=nums[n-1]*nums[n-2]*nums[n-3];
+        product2=nums[0]*nums[1]*nums[n-1];     
+        return Math.max(product1,product2);
+    }
+}
