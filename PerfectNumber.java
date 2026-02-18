70ec63b3da27264b290d2000242ff3940fef00ea
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-02-18 21:32:54 +0530

    Create PerfectNumber.java

---
+++ PerfectNumber.java
@@ -0,0 +1,12 @@
+public class PerfectNumber {
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i<num;i++){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        return sum==num;
+    }
+}
+
