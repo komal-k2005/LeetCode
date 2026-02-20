926df74e4d794b4f24dd1329ce5190cd9bebb489
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-02-20 11:01:33 +0530

    Create SquareOfSortedArray.java

---
+++ SquareOfSortedArray.java
@@ -0,0 +1,12 @@
+public class SquareOfSortedArray {
+    public int[] sortedSquares(int[] nums) {
+        int[] arr=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            arr[i]=nums[i]*nums[i];
+        }
+        int temp=0;
+        Arrays.sort(arr);
+        return arr;
+    }
+}
+
