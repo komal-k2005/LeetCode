800b034d3f551bffd6585e85c7466f1ca291d553
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-03-13 11:21:37 +0530

    Create TrimTrailingVowels.java

---
+++ TrimTrailingVowels.java
@@ -0,0 +1,16 @@
+public class TrimTrailingVowels {
+    public String trimTrailingVowels(String s) {
+        int i=s.length()-1;
+        while(i>=0){
+            int ch=s.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o'|| ch=='u'){
+                i--;
+            }
+            else{
+                break;
+            }
+        }
+        return s.substring(0,i+1);
+    }
+}
+
