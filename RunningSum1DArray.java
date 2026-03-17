c044be21ac0487b9224c4abb73987d65413ef1e4
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-03-17 21:26:15 +0530

    Create RunningSum1DArray.java

---
+++ RunningSum1DArray.java
@@ -0,0 +1,10 @@
+public class RunningSum1DArray {
+    public int[] runningSum(int[] nums) {
+        int[] arr=new int[nums.length];
+        arr[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            arr[i]=arr[i-1]+nums[i];
+        }
+        return arr;
+    }
+}
