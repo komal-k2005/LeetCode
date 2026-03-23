d7ff62f12f477ba495b30b6f1480dba3d83cda84
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-03-23 22:46:58 +0530

    Create ConvertTheTemperature.java

---
+++ ConvertTheTemperature.java
@@ -0,0 +1,8 @@
+public class ConvertTheTemperature {
+    public double[] convertTemperature(double celsius) {
+        double[] d=new double[2];
+        d[0]=celsius + 273.15;
+        d[1]=celsius * 1.80 +32.00;
+        return d;
+    }
+}
