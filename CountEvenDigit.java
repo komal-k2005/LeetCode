ab76de3c1150b870d17b8ad36edf0d89822bdd3a
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-03-24 21:51:14 +0530

    Create CountEvenDigit.java

---
+++ CountEvenDigit.java
@@ -0,0 +1,16 @@
+public class CountEvenDigit {
+    public int countEven(int num) {
+       int n=num;
+       int sum=0;
+        while(0<n){
+           sum+=n%10;
+           n/=10;
+        }
+        if(sum%2==0){
+            return num/2;
+        }
+        else{
+            return (num-1)/2;
+        }
+    }
+}
