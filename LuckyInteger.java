b3c3989c56fc253a8f0dfdfbd6e8629684cfc8db
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-03-30 18:00:15 +0530

    Create LuckyInteger.java

---
+++ LuckyInteger.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int n:arr){
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+        int res=-1;
+        for(int n:map.keySet()){
+            if(n==map.get(n)){
+                res=Math.max(res,n);
+            }
+        }
+        return res;
+        }
+}
