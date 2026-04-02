b87c37b391801c8ee0bf451f946e3a5c19a4a21c
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-04-02 22:00:12 +0530

    Create UniqueNumberOccurence.java

---
+++ UniqueNumberOccurence.java
@@ -0,0 +1,18 @@
+public class UniqueNumberOccurence {
+
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int n:arr){
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+        HashSet<Integer> set=new HashSet<>();
+
+        for(int v:map.values()){
+            if(set.contains(v)){
+                return false;
+            }
+            set.add(v);
+        }
+        return true;
+    }
+}
