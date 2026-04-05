d4d621ad3d709ab3e13cb42680feaa1caa12989a
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-04-05 18:41:53 +0530

    Create RobotReturnToOrgin.java

---
+++ RobotReturnToOrgin.java
@@ -0,0 +1,22 @@
+public class RobotReturnToOrgin {
+    public boolean judgeCircle(String moves) {
+        int x=0;
+        int y=0;
+        for(char c:moves.toCharArray()){
+            if(c == 'U'){
+                 x++;
+            }
+            else if(c == 'D'){
+                x--;
+            }
+            else if(c == 'R'){
+                y++;
+            }
+            else if(c== 'L'){
+                y--;
+            }
+        }
+        return x==0 && y==0;
+    }
+}
+    
