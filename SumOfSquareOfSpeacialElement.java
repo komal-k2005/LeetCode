ee8c1b55a40b9f5d4f856d8154b82373e23dc100
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-04-14 21:50:57 +0530

    Create SumOfSquareOfSpeacialElement.java

---
+++ SumOfSquareOfSpeacialElement.java
@@ -0,0 +1,12 @@
+public class SumOfSquareOfSpeacialElement {
+    public int sumOfSquares(int[] nums) {
+        int sum=0;
+        int n=nums.length;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                sum=sum+(nums[i-1]*nums[i-1]);
+            }
+        }
+        return sum;
+    }
+}
