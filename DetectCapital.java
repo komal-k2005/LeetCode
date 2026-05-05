0e854e2a9a77bd5555902b3479bb22249c5ab08d
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-05-05 22:17:38 +0530

    Create DetectCapital.java

---
+++ DetectCapital.java
@@ -0,0 +1,17 @@
+public class DetectCapital{
+    public boolean detectCapitalUse(String word) {
+        int count=0;
+        for(int i=0;i<word.length();i++){
+           char ch=word.charAt(i);
+           if(ch>='A' && ch<='Z'){
+            count++;
+           }
+        }
+        if(count==0) return true;
+        if(count==word.length()) return true; 
+        if(count==1 && word.charAt(0)>='A' && word.charAt(0)<='Z'){
+            return true;
+        }
+        return false;
+}
+}
