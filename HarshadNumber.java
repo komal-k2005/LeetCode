03c54d1b4000262426d5ec35920cf796401186c7
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-05-08 21:45:28 +0530

    Create HarshadNumber.java

---
+++ HarshadNumber.java
@@ -0,0 +1,14 @@
+public class HarshadNumber {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum=0;
+        int num=x;
+        while(x>0){
+            sum+=x%10;
+            x/=10;
+        }
+        if(num%sum==0){
+            return sum;
+        }
+        return -1;
+    }
+}
