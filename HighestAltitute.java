f3ed63fdd73579f5df8462cbd5efdf35d8284651
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-05-16 22:53:18 +0530

    Create HighestAltitute.java

---
+++ HighestAltitute.java
@@ -0,0 +1,11 @@
+public class HighestAltitute {
+    public int largestAltitude(int[] gain) {
+        int max=0;
+        int alt=0;
+        for(int i=0;i<gain.length;i++){
+            alt+=gain[i];
+            max=Math.max(max,alt);
+        }
+        return max;
+    }
+}
