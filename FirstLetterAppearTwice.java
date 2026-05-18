8b1997cc3ba730dcb812b1cf13463772b552f41a
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-05-18 09:59:09 +0530

    Create FirstLetterAppearTwice.java

---
+++ FirstLetterAppearTwice.java
@@ -0,0 +1,12 @@
+public class FirstLetterAppearTwice{
+    public char repeatedCharacter(String s) {
+        HashSet<Character> set=new HashSet<>();
+        for(char c:s.toCharArray()){
+            if(set.contains(c)){
+                return c;
+            }
+            set.add(c);
+        }
+        return ' ';
+    }
+}
