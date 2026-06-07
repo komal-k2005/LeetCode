60f9da7011c4bfcb170ac9a96dfbb430d0766bb0
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-06-07 11:13:56 +0530

    Create CountNumSpecialChar.java

---
+++ CountNumSpecialChar.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+public class CountNumSpecialChar{
+    public int numberOfSpecialChars(String word) {
+        HashSet<Character> lower=new HashSet<>();
+        HashSet<Character> upper=new HashSet<>();
+        for(char ch:word.toCharArray()){
+            if(Character.isLowerCase(ch)){
+                lower.add(ch);
+            }
+            else{
+                upper.add(Character.toLowerCase(ch));
+            }
+        }
+        int count=0;
+        for(char ch:lower){
+            if(upper.contains(ch)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
