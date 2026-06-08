e111838599f94da145911831aaccd6e040563deb
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-06-08 10:49:47 +0530

    Create NextGreaterElement.java

---
+++ NextGreaterElement.java
@@ -0,0 +1,22 @@
+public class NextGreaterElement {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] arr=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            int index=-1;
+            for(int j=0;j<nums2.length;j++){
+                if(nums2[j]==nums1[i]){
+                    index=j;
+                    break;
+                }
+            }
+            arr[i]=-1;
+            for(int j=index+1;j<nums2.length;j++){
+                if(nums2[j]>nums1[i]){
+                    arr[i]=nums2[j];
+                    break;
+                }
+            }
+        }
+        return arr;
+}
+}
