0682408e0704d40da54adfb7322ae5e79a47698a
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-06-09 14:54:53 +0530

    Create FurtherPointOrigin.java

---
+++ FurtherPointOrigin.java
@@ -0,0 +1,19 @@
+public class FurtherPointOrigin {
+    public int furthestDistanceFromOrigin(String moves) {
+        int left=0;
+        int right=0;
+        int blank=0;
+        for(int i=0;i<moves.length();i++){
+            if(moves.charAt(i)=='L'){
+                left++;
+            }
+            else if(moves.charAt(i)=='R'){
+                right++;
+            }
+            else{
+                blank++;
+            }
+        }
+        return Math.abs(right-left)+blank;
+    }
+}
