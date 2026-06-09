99fffdf6aa9bcca5a29e73966ebc753ebf1b9d3d
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-06-10 00:08:31 +0530

    Create MaximumTotalSubArrayValue.java

---
+++ MaximumTotalSubArrayValue.java
@@ -0,0 +1,11 @@
+public class MaximumTotalSubArrayValue {
+    public long maxTotalValue(int[] nums, int k) {
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            max=Math.max(nums[i],max);
+            min=Math.min(nums[i],min);
+        }
+        return  1L*k*(max-min);
+    }
+}
