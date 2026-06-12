1ce18c9676076990ab7bf68d7498454a95355921
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-06-12 12:20:54 +0530

    Create RansomNote.java

---
+++ RansomNote.java
@@ -0,0 +1,18 @@
+public class RansomNote {
+ public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length()>magazine.length()) {
+            return false;
+        }
+        int[] count=new int[26];
+        for(char ch:magazine.toCharArray()){
+            count[ch-'a']++;
+        }
+        for(char ch:ransomNote.toCharArray() ){
+            if(count[ch-'a']==0){
+                return false;
+            }
+            count[ch-'a']--;
+        }
+        return true;
+    }
+}
