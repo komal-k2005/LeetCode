a17e123fef773cc226e22f2d7b670dc8f5d823fc
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-06-30 11:09:05 +0530

    Create SubstringInWord.java

---
+++ SubstringInWord.java
@@ -0,0 +1,11 @@
+public class SubstringInWord {
+    public int numOfStrings(String[] patterns, String word) {
+        int count=0;
+        for(String s:patterns){
+            if(word.contains(s)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
