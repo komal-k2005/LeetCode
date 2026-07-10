0f535d2670f144bd9ad95cfb70c5dccc7e886339
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-07-10 17:48:09 +0530

    Create SlowestKey.java

---
+++ SlowestKey.java
@@ -0,0 +1,17 @@
+public class SlowestKey{
+    class Solution {
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
+        char ans=keysPressed.charAt(0);
+        int max=releaseTimes[0];
+        int duration=0;
+        for(int i=1;i<releaseTimes.length;i++){
+            duration=releaseTimes[i]-releaseTimes[i-1];
+            char ch=keysPressed.charAt(i);
+            if(duration>max || (duration==max && ans<ch)){
+                ans=ch;
+                max=duration;
+            }
+        }
+        return ans;
+    }
+}
