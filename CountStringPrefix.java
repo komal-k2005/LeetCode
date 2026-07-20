2517679caa5775883d034986b19716475ed01ddc
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-07-20 09:50:57 +0530

    Create CountStringPrefix.java

---
+++ CountStringPrefix.java
@@ -0,0 +1,11 @@
+public class ChackStringPrefix{
+    public int countPrefixes(String[] words, String s) {
+        int count=0;
+        for(String str:words){
+            if(s.startsWith(str)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
