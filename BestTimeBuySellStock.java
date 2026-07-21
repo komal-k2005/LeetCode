f09091db10440260dd837ef76e6b86e920a98508
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-07-21 09:10:40 +0530

    Create BestTimeBuySellStock.java

---
+++ BestTimeBuySellStock.java
@@ -0,0 +1,15 @@
+public class BestTimeBuySellStock{
+    public int maxProfit(int[] prices) {
+        int min=prices[0];
+        int max=0;
+        for(int n:prices){
+            if(n<min){
+                min=n;
+            }
+            else{
+                max=Math.max(max,n-min);
+            }
+        }
+        return max;
+    }
+}
