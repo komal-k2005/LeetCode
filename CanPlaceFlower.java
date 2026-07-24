4ee306176eebea0e2debcb25ce1065c0a612ef26
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-07-24 17:46:54 +0530

    Create CanPlaceFlower.java

---
+++ CanPlaceFlower.java
@@ -0,0 +1,13 @@
+public class CanPlaceFlower {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for(int  i=0;i<flowerbed.length;i++){
+            if((flowerbed[i]==0)&&
+            (i==0 || flowerbed[i-1]==0)
+            &&(flowerbed.length-1==i || flowerbed[i+1]==0)){
+                n--;
+                flowerbed[i]=1;
+            }
+        }
+        return n<=0;
+    }
+}
