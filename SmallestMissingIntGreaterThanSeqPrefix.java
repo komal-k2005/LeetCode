5e861942f98bcb326bfc117b0dea8c3e41b07e24
kathawde komal <125153190+komal-k2005@users.noreply.github.com>
2026-08-12 21:02:29 +0530

    Create SmallestMissingIntGreaterThanSeqPrefix.java

---
+++ SmallestMissingIntGreaterThanSeqPrefix.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+public class SmallestMissingIntGreaterThanSeqPrefix {
+    public int missingInteger(int[] nums) {
+        int sum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]+1){
+                sum+=nums[i];
+            }
+            else{
+                break;
+            }
+        }
+        HashSet<Integer> set=new HashSet<>();
+        for(int n:nums){
+            set.add(n);
+        }
+        while(set.contains(sum)){
+            sum++;
+        }
+        return sum;
+    }
+}
